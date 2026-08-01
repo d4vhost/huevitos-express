5b392dd3822dff700239c3a9f5a794e467820688
David <davidandradz7@gmail.com>
2026-08-01 01:17:34 -0500

    style: adjust desktop logo position to not crop head, and center on mobile

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -42,7 +42,7 @@ export default function Nosotros() {
                 src="/logo_huevos.jpg"
                 alt="Historia de Huevitos Express"
                 fill
-                style={{ objectFit: 'cover', objectPosition: 'center' }}
+                style={{ objectFit: 'cover', objectPosition: 'center 10%' }}
               />
             </div>
             <div>
